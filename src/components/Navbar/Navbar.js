429abec16d9b42424c703ae884508a9d9a26d822
balbano80 <balbano80@gmail.com>
2018-08-06 18:52:42 -0700

    corrected Clicky-Game navbar link

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = props =>(
         <div class="container">
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul id="menu-main-nav" class="navbar-nav nav-fill w-100">
-              <li class="nav-item mt-1"><a href="/"><h2>Clicky Game</h2></a></li>
+              <li class="nav-item mt-1"><a href="https://balbano80.github.io/mha-clicky-game/"><h2>Clicky Game</h2></a></li>
               <li class="nav-item mt-3"><h4>{props.display}</h4></li>
               <li class="nav-item mt-3"><h4>Score: {props.score} | Top Score: {props.topScore}</h4></li>
             </ul>
